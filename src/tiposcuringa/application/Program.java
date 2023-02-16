1ff199848fe370d288a87857403bc76b3f052064
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-02-16 18:34:40 -0300

    basic demostration of the wildcard types

---
+++ src/tiposcuringa/application/Program.java
@@ -0,0 +1,24 @@
+package tiposcuringa.application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		//demonstração de tipos curinga 
+		List<?> x = new ArrayList<Object>();
+		List<Integer> y = new ArrayList<Integer>();
+		x = y;
+
+		List<Integer> z = Arrays.asList(28, 18, 8);
+		printList(z);
+	}
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+		System.out.println(obj);
+		}
+		}
+}
