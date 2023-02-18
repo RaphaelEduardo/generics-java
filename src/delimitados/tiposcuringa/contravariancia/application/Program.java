3d6b4c35ed57b39558ad1d8bb5b9b09d0d2223b5
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-02-18 16:55:59 -0300

    get/put principle: counter variance

--- src/delimitados/tiposcuringa/contravariancia/application/Program.java
+++ src/delimitados/tiposcuringa/contravariancia/application/Program.java
@@ -6,12 +6,22 @@ import java.util.List;
 public class Program {
 
 	/*
-	 * PRINCIPIO GET / PUT
+	 * PRINCIPIO GET / PUT 
 	 * Contravariância
 	 */
 
 	public static void main(String[] args) {
+
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
 		
- 
+		// OK (PUT)
+		List<? super Number> myNums = myObjs;
+		myNums.add(10);
+		myNums.add(3.14);
+		
+		 // erro de compilacao (GET)
+		// Number x = myNums.get(0);
 	}
 }
